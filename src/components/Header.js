d8fda140c10e81b703add3e2ed47c208e6484101
Marco Kuang <marcojkuang@gmail.com>
2020-08-04 19:50:33 -0700

    Signout should be invidisible to unauthed user

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
 class Header extends React.Component {
+  renderLinks() {
+    if (this.props.authenticated) {
+      return <Link to="/signout">Sign Out</Link>;
+    } else {
+      return <Link to="/signin">Sign In</Link>;
+    }
+  }
+
   render() {
     return (
       <div>
         <Link to="/">Home</Link>
-        <Link to="/signin">Sign In</Link>
-        <Link to="/signout">Sign Out</Link>
+        {this.renderLinks()}
       </div>
     );
   }
 }
 
-export default Header;
+const mapStateToProps = (state) => {
+  return {
+    authenticated: state.auth.authenticated,
+  };
+};
+
+export default connect(mapStateToProps)(Header);
